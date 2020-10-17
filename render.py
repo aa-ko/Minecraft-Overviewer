5946fd252bd04762e4015e11a35ad39cd1c314c6
Aaron Korte <github@aaronkorte.xyz>
2020-10-17 13:21:38 +0200

    Make render thread count dynamic.

--- render.py
+++ render.py
@@ -1,13 +1,15 @@
 import subprocess
 import schedule
 import time
+import multiprocessing
 
 render_interval_in_minutes = 15
 cooldown_interval_in_seconds = 30
+thread_count = multiprocessing.cpu_count()
 
 def run_renderer():
     print("Running renderer..")
-    res = subprocess.run(["./overviewer.py", "--rendermodes=smooth-lighting", "-p12", "/world", "/cache"])
+    res = subprocess.run(["./overviewer.py", "--rendermodes=smooth-lighting", "-p%s"%thread_count, "/world", "/cache"])
     if res.returncode is not 0:
         print("Something went wrong when executing renderer.")
     else:
